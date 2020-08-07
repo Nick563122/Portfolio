9f2c7c59bb03cf2b650a1a796d5762d9e37d2029
unknown <luceronicholas0@gmail.com>
2020-08-07 16:22:22 -0600

    Updates

---
+++ precache-manifest.e6c6597d0744aabffee0f0c0cc570079.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d156b5c75a142ff73d19ec682b4318d4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "fd7ccc33334bc52735ff",
+    "url": "/Portfolio/static/css/main.54929711.chunk.css"
+  },
+  {
+    "revision": "0944e2143af354b930ce",
+    "url": "/Portfolio/static/js/2.9b5ef32c.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/Portfolio/static/js/2.9b5ef32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd7ccc33334bc52735ff",
+    "url": "/Portfolio/static/js/main.e28eb4d8.chunk.js"
+  },
+  {
+    "revision": "95694ffb7194c73ef8f9",
+    "url": "/Portfolio/static/js/runtime-main.5ac5bc46.js"
+  },
+  {
+    "revision": "54b77e20564bd5c27e94d4c72caa8ac6",
+    "url": "/Portfolio/static/media/Guess-the-Number.54b77e20.png"
+  },
+  {
+    "revision": "15ee7bbbe18c60c027e30222c8356c8f",
+    "url": "/Portfolio/static/media/Insectopedia.15ee7bbb.png"
+  },
+  {
+    "revision": "a6599ecf687d5a5c4537932f3a25ab41",
+    "url": "/Portfolio/static/media/ToDo-list.a6599ecf.png"
+  },
+  {
+    "revision": "e478732e271f266dccb84687f726484e",
+    "url": "/Portfolio/static/media/tic-tac-toe.e478732e.png"
+  }
+]);
